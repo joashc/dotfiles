bf881c4c9e9b8d4b51b3e1f804e4705be34372b1
joashc <joash.is@gmail.com>
2016-04-19 21:40:29 +1000

    Update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,78 +1,114 @@
-# Path to your oh-my-fish.
-set fish_path $HOME/.oh-my-fish
 set -gx LANG en_AU.UTF-8
 set -gx LC_ALL en_AU.UTF-8
 set -gx LC_CTYPE en_AU.UTF-8
-set -gx PATH /usr/local/bin $PATH
+set -g Z_SCRIPT_PATH /usr/lib/z.sh
 
-set fish_greeting " "
 
-# Theme
-set fish_theme agnoster
-function lso
-    pacman -Qdt
+# Path to Oh My Fish install.
+set -gx OMF_PATH "/home/boo/.local/share/omf"
+
+# Customize Oh My Fish configuration path.
+#set -gx OMF_CONFIG "/home/boo/.config/omf"
+
+# Load oh-my-fish configuration.
+source $OMF_PATH/init.fish
+
+set fish_plugins z
+
+function fish_user_key_bindings
+  fish_vi_key_bindings
 end
+
+
 function hd
   xrandr --output HDMI3 --mode 1920x1080
 end
 function uhd
   xrandr --output HDMI3 --mode 3840x2160
 end
+
 function rmo
-    sudo pacman -Rs (pacman -Qtdq)
+  sudo pacman -Rns (pacman -Qdtq)
 end
-function ipext
-    curl http://ipecho.net/plain ; echo
+function lso
+  pacman -Qdtq
 end
+
 function lt
-    ls -ltr $argv
+  ls -ltrh
 end
+
 function f
-    find . -iname $argv
+  find . -iname $argv
 end
+
 function em
-    emacsclient -t $argv
-end
-function sdn
-    sudo shutdown now
+  emacsclient -t $argv
 end
 function eme
-    emacsclient -c $argv &
+  emacsclient -c $argv &
 end
-function windows
-    VBoxManage startvm Windows
+
+function s
+  git status -s $argv
 end
 function co
-    git checkout $argv
+  git checkout $argv
 end
-function s
-    git status -s $argv
+function rb
+  git rebase $argv
 end
 function l
-    git lg $argv
+  git lg $argv
+end
+function mg
+  git merge $argv
+end
+function m
+  git commit -m $argv
+end
+function am
+  git commit -am $argv
+end
+function A
+  git add -A $argv
+end
+function rs
+  git reset $argv
+end
+function RSH
+  git reset --hard $argv
+end
+function com
+  git commit $argv
+end
+function con
+  git rebase --continue $argv
+end
+function amend
+  git commit --amend $argv
+end
+function p
+  git push $argv
+end
+function pl
+  git pull $argv
+end
+function a
+  git add $argv
+end
+function cfon
+  touch ~/.caf
+end
+function cfoff
+  rm ~/.caf
+end
+function win
+  VBoxManage startvm Windows10
 end
 
-function activate
-    sudo modprobe vboxdrv
-end
-
-# Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
-# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
-# Example format: set fish_plugins autojump bundler
-set fish_plugins autojump vi-mode
-
-# Path to your custom folder (default path is $FISH/custom)
-#set fish_custom $HOME/dotfiles/oh-my-fish
-
-# Load oh-my-fish configuration.
-. $fish_path/oh-my-fish.fish
-
-sh ./xcape &
-
-# start X at login
 if status --is-login
-    if test -z "$DISPLAY" -a $XDG_VTNR = 1
-        #exec startx
+    if test -z "$DISPLAY" -a $XDG_VTNR -eq 1
+        exec startx -- -keeptty
     end
 end
-
